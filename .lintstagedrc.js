67826233d864e8c99afd3d803b77da36f2b51d9f
maliksaepul <malik@inspigo.id>
2023-11-10 10:52:15 +0700

    fix(lint-staged): script format

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,9 +4,6 @@ module.exports = {
   // Lint & Prettify TS and JS files
   '**/*.(ts|tsx|js|mjs)': (filenames) => [
     `yarn lint . ${filenames.join(' ')}`,
-    `yarn prettier --write ${filenames.join(' ')}`,
+    `yarn prettier --write . ${filenames.join(' ')}`,
   ],
-  // Prettify only Markdown and JSON files
-  '**/*.(md|json)': (filenames) =>
-    `yarn prettier --write ${filenames.join(' ')}`,
 };
